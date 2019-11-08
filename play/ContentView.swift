134ea3813a7e07119fb7df32afd6aed77865546c
Eric Passmore <eric.passmore@gmail.com>
2019-11-08 15:23:49 -0800

    Initial Commit

---
+++ play/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  play
+//
+//  Created by Eric Passmore on 11/8/19.
+//  Copyright © 2019 OK2Save. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
